455960abfd3b74f54c3f4e78c4dfbd6ba05f7b8d
Michael Rode <michael.rode@workrise.com>
2024-05-15 23:34:26 -0400

    Working before refactor

--- api/create-branch-from-jira-ticket.js
+++ api/create-branch-from-jira-ticket.js
@@ -2,17 +2,27 @@ const { git } = require('cmd-executor')
 const memoize = require('fast-memoize')
 const changeCase = require('change-case')
 const axios = require('axios').default
+const { Version3Client } = require('jira.js')
+
+const client = new Version3Client({
+    host: 'https://workrise.atlassian.net',
+    newErrorHandling: true,
+    authentication: {
+        personalAccessToken:
+            'bWljaGFlbC5yb2RlQHdvcmtyaXNlLmNvbTpBVEFUVDN4RmZHRjBiT013OVVIWXRJOF9MbXFhVFVValpnaEpFbmRuNlgwMlVpZ2Y4RXpfOFNTR0s3TmZMTGxKNWR6bFk4Y20yR0RHc0ZUWXplOEhpd0M1aS1wQmJ0b3VYUVh6YUxycm5qdGdMUm85V0J4ejlpQWhYZTgwTEx2OW1xb3FrMC1jUmxjVnhMY2JGMVYyYW8taURTY3g1bmtjYzZWdTlNUW10ckxndlE4N3FHMnBOeE09Njc4MjA3RUU=',
+    },
+})
 
 require('dotenv').config()
 const currentWorkingDirectory = process.cwd()
-process.chdir('/Users/michael.rode/Code/ee/jira-api')
+process.chdir('/Users/michael.rode/code/general/jira-api')
 
 const createClient = (
-    baseURL = 'https://workrise.atlassian.net/rest/api/3'
+    baseURL = 'https://workrise.atlassian.net/rest/api/2/'
 ) => {
     const headers = {
         'Content-Type': 'application/json',
-        Authorization: `Basic ${process.env.JIRA_TOKEN}`,
+        Authorization: `Basic bWljaGFlbC5yb2RlQHdvcmtyaXNlLmNvbTpBVEFUVDN4RmZHRjBiT013OVVIWXRJOF9MbXFhVFVValpnaEpFbmRuNlgwMlVpZ2Y4RXpfOFNTR0s3TmZMTGxKNWR6bFk4Y20yR0RHc0ZUWXplOEhpd0M1aS1wQmJ0b3VYUVh6YUxycm5qdGdMUm85V0J4ejlpQWhYZTgwTEx2OW1xb3FrMC1jUmxjVnhMY2JGMVYyYW8taURTY3g1bmtjYzZWdTlNUW10ckxndlE4N3FHMnBOeE09Njc4MjA3RUU=`,
     }
 
     const axiosOptions = {
@@ -28,6 +38,7 @@ const jiraClient = createClient()
 const fetchIssueData = async jiraKey => {
     try {
         const { data } = await jiraClient.get(`issue/${jiraKey}`)
+
         return {
             key: data.key,
             name: data.fields.summary,
@@ -53,7 +64,10 @@ process.chdir(currentWorkingDirectory)
 const getJiraFields = async () => {
     const key =
         process.argv[2].match(/WRK.\d{3,6}/) ||
-        process.argv[2].match(/EE.\d{3,6}/)
+        process.argv[2].match(/EE.\d{3,6}/) ||
+        process.argv[2].match(/SRED.\d{3,6}/) ||
+        process.argv[2].match(/SRE23.\d{2,6}/)
+
     return await fetchIssueData(key[0])
 }
 const memoizedGetJiraFields = memoize(getJiraFields)
@@ -87,7 +101,9 @@ const formattedBranchName = async () => {
     return isNewBranch ? `-b ${branchName}` : branchName
 }
 ;(async () => {
+    // await runthis()
     const branchName = await formattedBranchName()
+
     await git.checkout(`${branchName}`)
 })().catch(error => {
     console.log('Error', error)
